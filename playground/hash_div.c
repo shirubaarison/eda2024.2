84efaba2b17adaeee1ce5e1146938c3c725958d8
Alysson Silva <shirubaarison@gmail.com>
2025-01-11 11:56:59 -0300

    Adicionar mains separados para testes

---
+++ playground/hash_div.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+#include "../include/common.h"
+#include "../include/div.h"
+
+int main(int argc, char *argv[])
+{  
+  if (argc < 2) {
+    printf("Uso: %s tam\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  int tam = atoi(argv[1]);
+  if (tam <= 0) {
+    printf("Tamanho não pode ser igual ou menor que 0");
+    return EXIT_FAILURE;
+  }
+  
+  Hash *tabela = criar_tabela();
+  
+  srand(time(NULL));
+  int i, valor;
+  for (i = 0; i < tam; i++) {
+    valor = rand() % TEST_MAX_CHAVES;
+    inserir_no(tabela, valor, tam, hash_div);
+    // printf("%d foi inserido na tabela.\n", valor);
+  }
+
+  printar_tabela(tabela, tam);
+
+  return EXIT_SUCCESS;
+}
